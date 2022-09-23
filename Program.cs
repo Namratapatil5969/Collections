2aa1237fc370c9071d3ace3700b30b2e4775fd64
namratapatil656@gmail.com <namratapatil656@gmail.com>
2022-09-23 15:37:34 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    //ArrayList list = new ArrayList();
+
+            //list.Add(10);
+            //list.Add(20);
+            //list.Add(30);
+            //list.Add(10);
+            //list.Insert(1, 15);
+            //list.IndexOf(10); // 0
+            //list.LastIndexOf(10);  //3
+            // list.Remove(30);
+            //list.RemoveAt(0);
+
+            //list.Sort();
+            //list.Reverse();
+            //list.Clear();
+            //Console.WriteLine($"Capacity of list is {list.Capacity}");
+            //Console.WriteLine(list.Count);
+            //foreach (var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            //Stack stack = new Stack();
+            //stack.Push(10);
+            //stack.Pop();
+            //stack.Peek();
+            //foreach (var item in stack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            //Queue q = new Queue();
+            //q.Enqueue(20);
+            //q.Dequeue();
+            //q.Peek();
+            //foreach (var item in q)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+        //    Hashtable ht = new Hashtable();
+        //    ht.Add(91, "IND");
+        //    ht.Add(1, "USA");
+        //    ht.Add(63, "AUS");
+        //    ht.Add(33, "FR");
+
+        //    ht.Remove(1);
+
+        //    foreach (DictionaryEntry d in ht)
+        //    {
+        //        Console.WriteLine($"{d.Key}  {d.Value}");
+        //    }
+        ////}
+
+    }
+
+}
+
---
+++ Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+//namespace Collections
+//{
+
+//    public class Student : IComparable
+//    {
+//        public string name;
+//        public int per;
+//        public Student(string name, int per)
+//        {
+//            this.name = name;
+//            this.per = per;
+//        }
+//        public int CompareTo(Object obj)
+//        {
+//            throw new NotImplementedException();
+//        }
+//        public override string ToString()
+//        {
+//            return $"Name:{name}   Percentage:{per}";
+//        }
+
+//    }
+//    public class abc : IEnumerable
+
+//    {
+//        Student[] students;
+//        public abc()
+//        {
+//            students = new Student[5];
+//            students[0] = new Student("Arya", 78);
+//            students[1] = new Student("Riya", 81);
+//            students[2] = new Student("Vidya", 87);
+//            students[3] = new Student("Neel", 92);
+//            students[4] = new Student("Raj", 75);
+//        }
+//        public IEnumerator GetEnumerator()
+//        {
+//            return students.GetEnumerator();
+//        }
+//    }
+//    public class program
+//    {
+//        static void Main(string[] args)
+//        {
+//            abc a1 = new abc();
+
+//            foreach (Student s in a1)
+//            {
+//                Console.WriteLine(s);
+//            }
+//        }
+//    }
+//}
+
+
+
+
